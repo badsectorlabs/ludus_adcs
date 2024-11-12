be478cf158ce51ab14bd7754008c895e74e66875
Brady McLaughlin <78229080+bradyjmcl@users.noreply.github.com>
2024-11-12 15:11:28 -0500

    build: 📦 included PSPKI module for ESC7 build
    included PSPKI from https://github.com/PKISolutions/PSPKI for use in ESC7 task. Credit given to original repo in files/PSPKI/README.md and ./README.md, respectively.
    
    Signed-off-by: Brady [independent contributor] <https://github.com/bradyjmcl>

---
+++ files/PSPKI/PSPKI/Server/Get-RequestArchivedKey.ps1
@@ -0,0 +1,26 @@
+function Get-RequestArchivedKey {
+<#
+.ExternalHelp PSPKI.Help.xml
+#>
+[OutputType('SysadminsLV.PKI.Cryptography.Pkcs.DefaultSignedPkcs7')]
+[CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [SysadminsLV.PKI.Management.CertificateServices.Database.AdcsDbRow[]]$Request
+    )
+
+    process {
+        foreach ($req in $Request) {
+            $CertAdmin = New-Object -ComObject CertificateAuthority.Admin
+            try {
+                $base64 = $CertAdmin.GetArchivedKey($req.ConfigString, $req.RowID, 1)
+                $bytes = [convert]::FromBase64String($base64)
+                New-Object SysadminsLV.PKI.Cryptography.Pkcs.DefaultSignedPkcs7 (,$bytes)
+            } catch {
+                Write-Error -Exception $_.Exception
+            } finally {
+                Release-COM $CertAdmin
+            }
+        }
+    }
+}
